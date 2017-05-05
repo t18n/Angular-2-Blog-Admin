9310cf389e3e29031333d12ab4ec692db40d5c34
Saroj Adhikari <sarojadhikarix@gmail.com>
2017-05-05 17:58:52 +0200

    Added panel and theme nice start :-)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeftsidebarComponent } from './leftsidebar/leftsidebar.component';
+import { RightsidebarComponent } from './rightsidebar/rightsidebar.component';
+import { PanelComponent } from './panel/panel.component';
+
+const routes: Routes = [
+  { path: '', component: PanelComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LeftsidebarComponent,
+    RightsidebarComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [ RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
